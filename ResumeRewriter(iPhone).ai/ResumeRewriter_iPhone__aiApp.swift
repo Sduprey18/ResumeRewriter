a7c5919602eaa598f9218afa18251a6ea8204e3e
John Doe <sherkeemduprey@gmail.com>
2025-04-11 16:01:32 -0400

    Initial Commit

---
+++ ResumeRewriter(iPhone).ai/ResumeRewriter_iPhone__aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeRewriter_iPhone__aiApp.swift
+//  ResumeRewriter(iPhone).ai
+//
+//  Created by Sherkeem Duprey on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ResumeRewriter_iPhone__aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
