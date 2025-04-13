718c196990ef965b78b34a1d6a5b85925d7a954c
Sherkeem Duprey <sherkeemduprey@gmail.com>
2025-04-12 20:53:20 -0400

    added some stuff, pdf api endpoint not working, internal server error

--- ResumeRewriter(iPhone).ai/backend/api.py
+++ ResumeRewriter(iPhone).ai/backend/api.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from pdf2image import convert_from_bytes, convert_from_path
+import pytesseract
+from io import BytesIO
+import base64
 
 app = FastAPI()
 
@@ -12,3 +16,25 @@ def root():
 def otherfunction():
     return("hi")
 
+@app.post("/uploadPDF")
+async def uploadPDF(file: UploadFile):
+    pdf_bytes = await file.read()
+    imageHolder = []
+
+    with BytesIO(pdf_bytes) as pdf:
+        #images = convert_from_path(pdf)
+        images = convert_from_bytes(pdf_bytes)
+        for i, img in enumerate(images):
+            img.save(f"page_{i + 1}.png", "PNG")
+            imageHolder.append(img)
+    
+    returnDict = {}
+
+    for i in range(len(imageHolder)):
+        with open(f"page_{i+1}.png", "rb") as image_file:
+           encoded_image_string = base64.b64encode(image_file.read()).decode("utf-8")
+           returnDict[f'page{i+1}'] = encoded_image_string
+
+    
+    return returnDict
+    
--- ResumeRewriter(iPhone).ai/backend/request.py
+++ ResumeRewriter(iPhone).ai/backend/request.py
@@ -1,6 +1,13 @@
 import requests 
 
 url = "http://127.0.0.1:8000/something"
+uploadURL = "http://127.0.0.1:8000/uploadPDF"
 x = requests.get(url)
 
-print(x.text)
+_file = {
+    'file': ('backend/sherkeemDupreyCurrentResume.pdf', open('backend/sherkeemDupreyCurrentResume.pdf', 'rb'), 'application/pdf')
+}
+
+response = requests.post(uploadURL, files=_file)
+
+print(response.text)
